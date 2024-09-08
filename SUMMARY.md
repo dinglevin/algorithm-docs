225b5f5607d6489d92926c776708e0d8e79172a5
dinglevin <dingweiqiong@gmail.com>
2024-09-08 23:42:42 +0800

    使用honkit组织文档结构

---
+++ SUMMARY.md
@@ -0,0 +1,14 @@
+# Summary
+
+* [Introduction](README.md)
+* [标题](docs/title.md)
+* [文本](docs/text.md)
+* [段落](docs/paragraph.md)
+* [数值](docs/number.md)
+* [标点符号](docs/marks.md)
+* [文档体系](docs/structure.md)
+* [参考链接](docs/reference.md)
+
+## License
+
+公共领域（public domain）
--- docs/.vuepress/config.js
+++
@@ -1,9 +0,0 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-  base: "/algorithm-docs/",
-  bundler: viteBundler(),
-  theme: defaultTheme(),
-})
